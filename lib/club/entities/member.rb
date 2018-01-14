b2121f49a79a86eb6de6fb2021038c82d71bef21
nerdinand <nerdinand@nerdinand.com>
2018-01-14 10:25:40 +0100

    Fix circular dependencies

--- lib/club/entities/member.rb
+++ lib/club/entities/member.rb
@@ -1,3 +1,6 @@
+require_relative './team'
+require_relative './group'
+
 class Member < Hanami::Entity
   attributes do
     attribute :group,   Types::Entity(Group)
--- lib/club/entities/team.rb
+++ lib/club/entities/team.rb
@@ -1,3 +1,6 @@
+class Member < Hanami::Entity
+end
+
 class Team < Hanami::Entity
   attributes do
     attribute :members, Types::Collection(Member)
